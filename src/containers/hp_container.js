401815c2500ad05658d175d31968ed597bdd914a
kaluzsikc <kaluzsikc@gmail.com>
2018-10-31 10:37:54 +0000

    Added reguest to API

--- src/containers/hp_container.js
+++ src/containers/hp_container.js
@@ -5,13 +5,27 @@ class HpContainer extends Component{
     constructor(props){
         super(props);
         this.state = {
-            characters:[]
+            characters:[],
+            url: "http://hp-api.herokuapp.com/api/characters"
         }
 
         //bind
     }
 
+    request(url){
+        fetch(url)
+        .then(response => response.json())
+        .then(ourData => this.setState({characters: ourData}))
+        .catch(error => console.log(error))
+    }
+
+    componentDidMount(){
+        this.request(this.state.url)
+       
+    }
+
     render(){
+        console.log(this.state.characters);
         return( 
             <div>
                 <p>
